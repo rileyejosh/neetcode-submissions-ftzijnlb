398822eaa09b1a25c7565ff01c115144e591c682
Josh <rileyejosh@outlook.com>
2026-05-07 21:13:43 -0500

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const results = new Array(nums.length);
+        const pre = new Array(nums.length);
+        const suf = new Array(nums.length);
+        pre[0] = 1;
+        let prod = nums[0];
+        for(let i = 1; i < nums.length; i++) {
+            pre[i] = prod;
+            prod *= nums[i];
+        }
+        suf.fill(1);
+        console.log(pre);
+        prod = 1;
+        suf[nums.length-1] = nums[nums.length-1];
+        // [48, 24, 6, 1]
+        for(let i = nums.length-1; i >= 0; i--) {
+            suf[i] = prod;
+            prod *= nums[i];
+        }
+        console.log(suf);
+
+        for(let i = 0; i < results.length; i++) {
+            results[i] = pre[i] * suf[i];
+        }
+
+        return results;
+    }
+}
