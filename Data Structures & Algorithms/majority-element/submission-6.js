a37611e77589a32a234a5db37a9e98b6a3b768ea
Josh <rileyejosh@outlook.com>
2026-07-29 11:28:38 -0500

    Add: majority-element - submission-6

---
+++ Data Structures & Algorithms/majority-element/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        const counts = new Map();
+        const uniqueNums = new Set();
+        let res = nums[0];
+        for(let n of nums) {
+            uniqueNums.add(n);
+            counts.set(n, (counts.get(n) || 0) + 1);
+        }
+
+        for(let n of uniqueNums) {
+            if(counts.get(n) > (nums.length/2)) {
+                res = n;
+                break;
+            }
+        }
+
+        return res;
+
+    }
+}
