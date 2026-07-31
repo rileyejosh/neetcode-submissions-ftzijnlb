bb3f8b61d8dbd993c0245e52b96d912d0047facb
Josh <rileyejosh@outlook.com>
2026-07-31 16:15:16 -0500

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    firstUniqChar(s) {
+        const unique = new Set();
+        const counts = new Map();
+
+        for(let c of s) {
+            
+            unique.add(c);
+            counts.set(c, (counts.get(c) || 0) + 1);
+        }
+
+        for(let i = 0; i < s.length; i++) {
+            let c = s[i];
+            if(counts.get(c) == 1) 
+                return i;
+        }
+
+        return -1;
+    }
+}
